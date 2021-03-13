d04c8df9caca431c7316915008c8382dcb4d14fe
cainy-a <cainy-a@users.noreply.github.com>
2021-03-13 14:16:48 +0000

    Take pieces

---
+++ Shared/ChessGame.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorChess.Shared
+{
+	public class ChessGame
+	{
+		public ChessBoard   Board        = ChessBoard.DefaultChessBoard;
+		public IList<Piece> TakenByWhite = new List<Piece>();
+		public IList<Piece> TakenByBlack = new List<Piece>();
+	}
+}
