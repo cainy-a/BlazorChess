b478565abe54d74839c2e33249b3ecd5524eb618
cainy-a <cainy-a@users.noreply.github.com>
2021-03-13 12:31:05 +0000

    Drag and drop

---
+++ Shared/ChessProcessor.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorChess.Shared
+{
+	public static class ChessProcessor
+	{
+		public static bool MoveIsValid(ChessBoard board, int startIndex, int endIndex)
+		{
+			return true; // TODO: implement the rules of chess. Oh god help.
+		}
+	}
+}
